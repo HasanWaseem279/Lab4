2ae0692d60f4886b1c2115eefc3777d79d167e9c
HasanWaseem279 <hasanwaseem2005@gmail.com>
2024-09-14 00:23:03 +0500

    Update Q9.c

--- Q9.c
+++ Q9.c
@@ -3,31 +3,39 @@
 int main() {
     float fsc_marks, nts_marks;
 
-    printf("Enter your F.Sc marks percentage: ");
+    printf("Enter F.Sc marks percentage: ");
     scanf("%f", &fsc_marks);
-    printf("Enter your NTS marks percentage: ");
+    printf("Enter NTS marks percentage: ");
     scanf("%f", &nts_marks);
 
-    printf("\nChecking eligibility for Oxford University:\n");
-    if (fsc_marks > 70 && nts_marks >= 70) {
-        printf("Eligible for Oxford University - IT Department\n");
-    } else if (fsc_marks > 70 && nts_marks >= 60) {
-        printf("Eligible for Oxford University - Electronics Department\n");
-    } else if (fsc_marks > 70 && nts_marks >= 50) {
-        printf("Eligible for Oxford University - Telecommunication Department\n");
-    } else {
-        printf("Not eligible for any department at Oxford University.\n");
+    printf("\nEligibility for Oxford University:\n");
+    
+    if(fsc_marks > 70 && nts_marks >= 70) {
+        printf("You can apply to Oxford IT Department\n");
+    }
+    else if(fsc_marks > 70 && nts_marks >= 60) {
+        printf("You can apply to Oxford Electronics Department\n");
+    }
+    else if(fsc_marks > 70 && nts_marks >= 50) {
+        printf("You can apply to Oxford Telecommunication Department\n");
+    }
+    else {
+        printf("Not eligible for Oxford\n");
     }
 
-    printf("\nChecking eligibility for MIT:\n");
-    if (fsc_marks >= 60 && nts_marks >= 50) {
-        printf("Eligible for MIT - IT Department\n");
-    } else if (fsc_marks >= 50 && fsc_marks <= 59 && nts_marks >= 50) {
-        printf("Eligible for MIT - Chemical Department\n");
-    } else if (fsc_marks > 40 && fsc_marks < 50 && nts_marks >= 50) {
-        printf("Eligible for MIT - Computer Department\n");
-    } else {
-        printf("Not eligible for any department at MIT.\n");
+    printf("\nEligibility for MIT:\n");
+    
+    if(fsc_marks >= 60 && nts_marks >= 50) {
+        printf("You can apply to MIT IT Department\n");
+    }
+    else if(fsc_marks >= 50 && fsc_marks <= 59 && nts_marks >= 50) {
+        printf("You can apply to MIT Chemical Department\n");
+    }
+    else if(fsc_marks > 40 && fsc_marks < 50 && nts_marks >= 50) {
+        printf("You can apply to MIT Computer Department\n");
+    }
+    else {
+        printf("Not eligible for MIT\n");
     }
 
     return 0;
